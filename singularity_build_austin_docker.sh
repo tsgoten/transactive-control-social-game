75611335d5ceff59d249620e766c6d06f2715407
Lucas <lucas.ingamba@gmail.com>
2021-12-07 21:38:14 -0800

    build singularity cache

--- singularity_build_austin_docker.sh
+++ singularity_build_austin_docker.sh
@@ -26,11 +26,31 @@
 # Run 8 examples concurrently
 #SBATCH --array=0-2
 
+BASE_DIR=/global/scratch/users/$USER
+
+LDIR=$BASE_DIR/.local$SLURM_ARRAY_TASK_ID
+LOGDIR_BASE=$BASE_DIR/logs
+
+
+rm -rf $LDIR
+mkdir -p $LDIR
+
+rm -rf /global/home/users/lucas_spangher/.cache/pip
+
+SINGULARITY_IMAGE_LOCATION=/global/scratch/users/$USER
+
+SINGULARITY_CACHEDIR=$BASE_DIR/.singularity/cache
+export SINGULARITY_CACHEDIR=$BASE_DIR/.singularity/cache
+
+SINGULARITY_TEMPDIR=$BASE_DIR/tmp
+export SINGULARITY_TEMPDIR=$BASE_DIR/tmp
+
 SINGULARITY_CACHE_DIR=/global/scratch/users/lucas_spangher/transactive-control-social-game
 
 if test -f austin_docker.sif; then
     echo "docker image exists"
-    singularity exec --nv --workdir ./tmp --bind $(pwd):$HOME --bind "$LDIR:$HOME/.local" sh -c "python feudal_trainer.py"
 else
     singularity build austin_docker.sif library://yanlarry/default/singularity_phnet:v1
 fi
+
+singularity exec --nv --workdir ./tmp --bind $(pwd):$HOME --bind "$LDIR:$HOME/.local" sh -c "python feudal_trainer.py"
