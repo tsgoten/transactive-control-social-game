2c8b2fd07d4aa6f98c1141256248ca7dceb3491b
Lucas <lucas.ingamba@gmail.com>
2021-12-14 23:21:56 -0800

    wandb group added

--- experiment_script.sh
+++ experiment_script.sh
@@ -1,11 +1,11 @@
 bash singularity_preamble_new.sh
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=directional -wandb_group=social_game_rewards &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l1 -wandb_group=social_game_rewards &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l2 -wandb_group=social_game_rewards &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l1 -wandb_group=social_game_rewards
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=directional -wandb_group=social_game_rewards &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 -wandb_group=social_game_rewards &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l2 -wandb_group=social_game_rewards &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 -wandb_group=social_game_rewards
 
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=directional -wandb_group=social_game_rewards &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l1 -wandb_group=social_game_rewards &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l2 -wandb_group=social_game_rewards &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l1 -wandb_group=social_game_rewards
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=directional -wandb_group=social_game_rewards &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 -wandb_group=social_game_rewards &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l2 -wandb_group=social_game_rewards &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 -wandb_group=social_game_rewards
 
