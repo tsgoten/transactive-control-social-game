f29b636706b393880bd97b2d3c5e37bab802335a
Lucas <lucas.ingamba@gmail.com>
2021-12-15 12:47:00 -0800

    home base experiment scripts

--- microgrid_experiment_script.sh
+++ microgrid_experiment_script.sh
@@ -1,3 +1,2 @@
-bash singularity_preamble_new.sh
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_spatial --lower_level_reward_type=directional -wandb_group=microgrid_test &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_spatial --lower_level_reward_type=l1 -wandb_group=microgrid_test &
+bash singularity_preamble_new.sh && python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_spatial --lower_level_reward_type=directional -wandb_group=microgrid_test &
+bash singularity_preamble_new.sh && python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_spatial --lower_level_reward_type=l1 -wandb_group=microgrid_test &
