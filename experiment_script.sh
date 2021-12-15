de75d73d6f35500084ef54ea68ed3c824e7f2057
lucas_spangher <lucas.ingamba@gmail.com>
2021-12-15 12:14:07 -0800

    experiment scripts

--- experiment_script.sh
+++ experiment_script.sh
@@ -1,4 +1,3 @@
-bash singularity_preamble_new.sh
 python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=directional -wandb_group=social_game_rewards &
 python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 -wandb_group=social_game_rewards &
 python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l2 -wandb_group=social_game_rewards &
---
+++ microgrid_experiment_script.sh
@@ -0,0 +1,1 @@
+bash singularity_preamble_new.sh && python feudal_trainer.py -w --numsteps=50000 --gym_env=feudal_spatial --wandb_group=microgrid_test
