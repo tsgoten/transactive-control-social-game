08ceb2e03f554ca7b24eb7e555569b9067e5cd56
Lucas <lucas.ingamba@gmail.com>
2021-12-14 19:59:44 -0800

    execute command

--- singularity_execute_austin_docker.sh
+++ singularity_execute_austin_docker.sh
@@ -53,5 +53,8 @@ else
     singularity build austin_docker.sif library://yanlarry/default/singularity_phnet:v1
 fi
 
-singularity run --nv --workdir ./tmp --bind $(pwd):$HOME --bind "$LDIR:$HOME/.local" austin_docker.sif sh -c "bash singularity_preamble_new.sh"
-singularity run --nv --workdir ./tmp --bind $(pwd):$HOME --bind "$LDIR:$HOME/.local" austin_docker.sif sh -c "bash experiment_script.sh"
+singularity run --nv --workdir ./tmp --bind $(pwd):$HOME --bind "$LDIR:$HOME/.local" austin_docker.sif sh -c "bash singularity_preamble_new.sh && /
+            python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=directional && /
+            python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l1 && /
+            python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l2 && /
+            python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_reward_function=l1"
