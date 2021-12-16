6db2996027839858656efcb24fc6cf7c272b6fef
Lucas <lucas.ingamba@gmail.com>
2021-12-15 17:13:07 -0800

    fixing typo in microgrid

--- microgrid_experiment_script.sh
+++ microgrid_experiment_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python feudal_trainer.py -w --numsteps=50000 --gym_env=feudal_spatial --wandb_group=final_microgrid_test &
-python feudal_trainer.py -w --numsteps=50000 --gym_env=feudal_spatial_lower_baseline --wandb_group=final_microgrid_test
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_spatial --wandb_group=final_microgrid_test &
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_spatial_lower_baseline --wandb_group=final_microgrid_test
