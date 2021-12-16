a22f1791046e5f2c331cd6ec5aa5d67d57004561
Lucas <lucas.ingamba@gmail.com>
2021-12-15 20:51:31 -0800

    experiment_script.sh

--- experiment_script.sh
+++ experiment_script.sh
@@ -3,9 +3,8 @@
 python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=directional --wandb_group=final_runs &
 python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 --wandb_group=final_runs &
 python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l2 --wandb_group=final_runs &
-python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 --wandb_group=final_runs
+python feudal_trainer.py -w --num_steps=50000 --gym_env=feudal_timewise --lower_level_reward_type=l1 --wandb_group=final_runs &
 
 
-python feudal_trainer.py -w --num_steps=50000 --gym_env=socialgame_env --lower_level_reward_type=directional --wandb_group=final_runs &
-s
+python feudal_trainer.py -w --num_steps=50000 --gym_env=socialgame_env --wandb_group=final_runs 
 
