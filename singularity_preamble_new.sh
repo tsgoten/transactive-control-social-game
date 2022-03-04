106d83157850ff7d58dd69c6a41b42b3775e78ad
Demosthen <1008981@gmail.com>
2022-03-03 18:01:05 -0800

    Add more singularity stuff

---
+++ singularity_preamble_new.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd gym-microgrid/
+pip install -e .
+cd ..
+cd gym-socialgame
+pip install -e .
+cd ..
+pip install tensorflow
+pip install tensorflow-probability
+pip install tensorflow-gpu
