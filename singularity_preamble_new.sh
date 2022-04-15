d0a64292237d49e11e9b38710400f4be4fdfd02b
Demosthen <1008981@gmail.com>
2022-04-15 07:37:20 +0000

    add locking for singularity packages? (again)

--- singularity_preamble_new.sh
+++ singularity_preamble_new.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+lockfile packages.lock
 cd gym-microgrid/
 pip install -e .
 cd ..
 cd gym-socialgame
 pip install -e .
 cd ..
+rm -f packages.lock
 #pip install tensorflow
 #pip install keras
 #pip install tensorflow-probability
