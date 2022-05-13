3274820ab777783dd94fe5604c9717c32a8b8eaa
Larry Yan <yanlarry@berkeley.edu>
2022-05-13 14:52:28 -0700

    table making script

--- writeup/run_queries.py
+++ writeup/run_queries.py
@@ -20,4 +20,34 @@ QUERIES = {
             "norl_complex": {"$or": [
                         {"config.tag": "complex_norl10"},]
                     },
+            "all" :   {"$or": [
+                        {"config.tag": "simple_baseline_custom5_alt"},
+                        {"config.tag": "medium_baseline_custom5_alt"},
+                        {"config.tag": "complex_baseline_custom5_alt"},
+                        {"config.tag": "simple_baseline_custom10_alt"},
+                        {"config.tag": "medium_baseline_custom10_alt"},
+                        {"config.tag": "complex_baseline_custom10_alt"},
+                        {"config.tag": "simple_baseline_custom20_alt"},
+                        {"config.tag": "medium_baseline_custom20_alt"},
+                        {"config.tag": "complex_baseline_custom20_alt"},
+                        {"config.tag": "simple_afl_custom5_alt"},
+                        {"config.tag": "medium_afl_custom5_alt"},
+                        {"config.tag": "complex_afl_custom5_alt"},
+                        {"config.tag": "simple_afl_custom10_alt"},
+                        {"config.tag": "medium_afl_custom10_alt"},
+                        {"config.tag": "complex_afl_custom10_alt"},
+                        {"config.tag": "simple_afl_custom20_alt"},
+                        {"config.tag": "medium_afl_custom20_alt"},
+                        {"config.tag": "complex_afl_custom20_alt"},
+                        {"config.tag": "simple_hnet_custom5_alt"},
+                        {"config.tag": "medium_hnet_custom5_alt"},
+                        {"config.tag": "complex_hnet_custom5_alt"},
+                        {"config.tag": "simple_hnet_custom10_alt"},
+                        {"config.tag": "medium_hnet_custom10_alt"},
+                        {"config.tag": "complex_hnet_custom10_alt"},
+                        {"config.tag": "simple_hnet_custom20_alt"},
+                        {"config.tag": "medium_hnet_custom20_alt"},
+                        {"config.tag": "complex_hnet_custom20_alt"},
+                        ]
+                    }
 }
